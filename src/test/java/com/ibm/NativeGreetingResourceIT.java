cd9399c9273668daff1cbfa6013d436a58e31c61
Jorge Raze <jorge.alcaraz@ibm.com>
2022-03-02 15:58:53 -0600

    Add database

--- src/test/java/com/ibm/NativeGreetingResourceIT.java
+++ src/test/java/com/ibm/NativeGreetingResourceIT.java
@@ -3,7 +3,7 @@ package com.ibm;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeGreetingResourceIT extends CardTransactionEndpointTest {
 
     // Execute the same tests but in native mode.
 }
