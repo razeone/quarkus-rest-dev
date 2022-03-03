15728a68cb4612590b2b6def692aaabb5161e369
Jorge Raze <jorge.alcaraz@ibm.com>
2022-03-03 10:35:22 -0600

    Update some stuff and remove other

--- src/test/java/com/ibm/NativeCardTransactionResourceIT.java
+++ src/test/java/com/ibm/NativeCardTransactionResourceIT.java
@@ -3,7 +3,7 @@ package com.ibm;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends CardTransactionEndpointTest {
+public class NativeCardTransactionResourceIT extends CardTransactionEndpointTest {
 
     // Execute the same tests but in native mode.
 }
