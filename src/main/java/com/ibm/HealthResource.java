47479270b0611795adc39ca7c7de6772bfb8e3ad
Jorge Raze <jorge.alcaraz@ibm.com>
2022-03-05 14:06:27 -0600

    Remove one code smell

--- src/main/java/com/ibm/HealthResource.java
+++ src/main/java/com/ibm/HealthResource.java
@@ -4,18 +4,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/health")
 public class HealthResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Status health() {
-        return new Status();
+    public Response health() {
+        return Response.ok(Status.STATUS_STRING).build();
     }
     
     public static class Status {
-        public String status = "UP";
+        
+        private Status() {
+        }
+
+        public static final String STATUS_STRING = "{\"status\":\"UP\"}";
     }
     
 }
